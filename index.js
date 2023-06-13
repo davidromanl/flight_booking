2d7503eae6113e4f85d441da793bd02ba0b6335e
David Roman <roman.david@gmail.com>
2023-06-13 17:38:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./server/app")
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(" Server runinig..");
+  console.log(" Server runinig...");
 });
